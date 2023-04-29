28320b45e274c551c0570267a8db1edd97635d09
Satori <bl00dztr34m@gmail.com>
2023-04-29 20:07:41 -0300

    agrego alumnoclases y agrego planificacionclases

--- backend/main/models/clase.py
+++ backend/main/models/clase.py
@@ -4,13 +4,30 @@ import json
 from datetime import datetime
 
 
-#Se define la tabla intermedia
+#Se define la tabla intermedia entre profesor y clases
 
 ProfesorClases = db.Table("profesorclases",
                           
     db.Column("id_Clase",db.Integer,db.ForeignKey("clase.id_Clase"),primary_key=True),
     db.Column("id_Profesor",db.Integer,db.ForeignKey("usuario_profesor.id_Profesor"),primary_key=True)
     )
+#Se define la tabla intermedia entre alumno y clases
+
+AlumnoClases = db.Table("alumnoclases",
+                          
+    db.Column("id_Clase",db.Integer,db.ForeignKey("clase.id_Clase"),primary_key=True),
+    db.Column("id_Alumno",db.Integer,db.ForeignKey("usuarios_alumnos.id_Alumno"),primary_key=True)
+    )
+
+#Se define la tabla intermedia entre planificacion y clases
+PlanificacionClases = db.Table("planificacionclases",
+                          
+    db.Column("id_Clase",db.Integer,db.ForeignKey("clase.id_Clase"),primary_key=True),
+    db.Column("id_planificacion",db.Integer,db.ForeignKey("planificacion.id_planificacion"),primary_key=True)
+    )
+
+
+
 
 
 class Clase(db.Model):
@@ -20,9 +37,9 @@ class Clase(db.Model):
     dia = db.Column(db.String(15), nullable=False)
     horaFin = db.Column(db.DateTime, nullable=False)
     horaInicio = db.Column(db.DateTime, nullable=False)
-    profesorclases = db.relationship('UsuarioProfesor', secondary=ProfesorClases, backref=db.backref('clases', lazy='dynamic'))
-    
-
+    profesorclases = db.relationship('UsuarioProfesor', secondary=ProfesorClases, backref=db.backref('clasesprofesor', lazy='dynamic'))
+    alumnoclases = db.relationship('UsuariosAlumnos', secondary=AlumnoClases, backref=db.backref('clasesalumno', lazy='dynamic'))
+    planificacionclases = db.relationship('Planificacion', secondary=PlanificacionClases, backref=db.backref('claseplanificacion', lazy='dynamic'))
 
     def __repr__(self):
         return '<Clase: %r >' % (self.detalles)

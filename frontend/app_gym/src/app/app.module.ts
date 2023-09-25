bc24aa1ccb4d3f24a88a3bbd3474db5fe991b0c9
Satori <bl00dztr34m@gmail.com>
2023-09-25 20:51:01 -0300

    agrego historia y profesores

--- frontend/app_gym/src/app/app.module.ts
+++ frontend/app_gym/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoriaComponent } from './pages/historia/historia.component';
+import { ProfesoresComponent } from './pages/profesores/profesores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HistoriaComponent,
+    ProfesoresComponent
   ],
   imports: [
     BrowserModule,

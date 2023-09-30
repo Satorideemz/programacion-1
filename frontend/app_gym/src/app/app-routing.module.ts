f8949265e5fde00b58f74fc7ffa08a2bcac0176b
Satori <bl00dztr34m@gmail.com>
2023-09-30 00:30:43 -0300

    agrego ruta nueva rutina admin

--- frontend/app_gym/src/app/app-routing.module.ts
+++ frontend/app_gym/src/app/app-routing.module.ts
@@ -43,6 +43,7 @@ import { HomeAdmiComponent } from './pages/admi/homeadmi/homeadmi.component';
 import { PagosAdmiComponent } from './pages/admi/pagos-admi/pagos-admi.component';
 import { PagosPuntualAdmiComponent } from './pages/admi/pagos-puntual-admi/pagos-puntual-admi.component';
 import { RutinaAbmAdmiComponent } from './pages/admi/rutina-abm-admin/rutina-abm-admin.component';
+import { NuevaRutinaAdmiComponent } from './pages/admi/nueva-rutina-admi/nueva-rutina-admi.component';
 
 
 const routes: Routes = [
@@ -91,6 +92,7 @@ const routes: Routes = [
   {path: 'pagos-admi', component: PagosAdmiComponent },
   {path: 'pagos-puntual-admi', component: PagosPuntualAdmiComponent },
   {path: 'rutina-abm-admi', component: RutinaAbmAdmiComponent},
+  {path: 'nueva-rutina-profesor', component: NuevaRutinaAdmiComponent },
 
 
 

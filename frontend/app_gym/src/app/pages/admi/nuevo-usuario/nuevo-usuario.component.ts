168554834048b27af8bbb24c1705e8897807b985
Carla Sofia Centeleghe <81233915+Carla-Sofia-Centeleghe@users.noreply.github.com>
2023-10-09 04:36:09 -0300

    formularios de ingreso y abm usuario, hacemos sus validaciones para que si o si deban rellenar los campos
    Preguntar como hacer para agregar mas validaciones, porque no nos funcionan

--- frontend/app_gym/src/app/pages/admi/nuevo-usuario/nuevo-usuario.component.ts
+++ frontend/app_gym/src/app/pages/admi/nuevo-usuario/nuevo-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevo-usuario',
@@ -6,4 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuevo-usuario.component.css']
 })
 export class NuevoUsuarioComponent {
+  nuevousuarioForm!: FormGroup;
+
+  constructor (
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.nuevousuarioForm = this.formBuilder.group({
+      nombre : ['', Validators.required],       //Validators.pattern('/^[A-Za-z\s']+$/')
+      apellido: ['', Validators.required],
+      mail : ['', Validators.required],         //Validators.email
+      telefono : ['', Validators.required],     //Validators.pattern('/^\d{10}$/')
+      documento : ['', Validators.required],    //Validators.pattern('/^\d{10}$/')
+      edad : ['', Validators.required],         //Validators.pattern('/^\d{10}$/')
+      sexo : ['', Validators.required],
+      titulo : ['', Validators.required],
+      disponibilidad : ['', Validators.required],
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required],
+    })
+  }
+
+  submit() {
+    if(this.nuevousuarioForm.valid) {
+      console.log('Form nuevo usario: ',this.nuevousuarioForm.value);
+      this.router.navigateByUrl('abm-usuario')
+      //this.login(this.nuevousuarioForm.value)
+    } else {
+      alert('Formulario no completo');
+    }
+  }
 }
+
